21cf31f89cdf054facd1899c3b2a3ccc26adb4b9
Jemuovic Djordje <jemuovicdjordje98@gmail.com>
2023-07-09 21:32:23 +0200

    Simple Project updata 1

--- src/MainPage/Components/SingleProject/index.jsx
+++ src/MainPage/Components/SingleProject/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import 'style.scss';
+import './style.scss';
 
 const SimpleProject = () => {
+    return (
+        <div>
+            <h1>01|Project title</h1>
+            <p>Deciption</p>
+            <p>Tehnologies</p>
+            <img src="#" alt="Project-Img" />
+        </div>
+    )
 
 }
 export default SimpleProject;
--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,12 +1,14 @@
 import './style.scss';
 import Navigation from './Components/Navigation';
 import Hero from './Components/Hero';
+import SimpleProject from './Components/SingleProject';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Hero />
+      <SimpleProject />
     </div>
   );
 }
