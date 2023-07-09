d356b2eb9504ba9993120aafc3a4eb0e895c1df8
Jovan Medarovic <end54065@adobe.com>
2023-07-09 18:50:52 +0200

    hero init

---
+++ src/MainPage/Components/Hero/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import arrowDown from '../../../resources/arrow-down.png'
+import './style.scss';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-text'>
+        <h1>Hello, I'm Djordje Jemuovic</h1>
+        <h2>A junior front end ReactJS developer who trying to specialize in all FrontEnd Web technologies</h2>
+      </div>
+      <img src={arrowDown} alt="arrow-down didn't loaded" />
+    </div>
+  );
+};
+
+export default Hero;
--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import Hero from './Components/Hero'
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      ASD TEST
+      <Hero />
     </div>
   );
 }
