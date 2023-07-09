f0df969e61fb325ee645312db6eb82d19bd139e8
Jovan Medarovic <end54065@adobe.com>
2023-07-09 18:53:44 +0200

    Removed implementation

--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Hero from './Components/Hero'
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+      ASD TEST
     </div>
   );
 }
